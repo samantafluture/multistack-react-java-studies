f033a324904a5ec603499b18a402b42231cac529
Samanta Fluture <samantafluture@gmail.com>
2021-11-05 13:31:42 -0200

    add files and folders from week 2

--- semana-2/basics/br/com/treinaweb/Collections.java
+++ semana-2/basics/br/com/treinaweb/Collections.java
@@ -1,4 +1,4 @@
-package br.com.treinaweb.semana2;
+package br.com.treinaweb;
 
 import java.util.ArrayList;
 import java.util.HashMap;
--- semana-2/basics/br/com/treinaweb/OperadoresLogicos.java
+++ semana-2/basics/br/com/treinaweb/OperadoresLogicos.java
@@ -1,4 +1,4 @@
-package br.com.treinaweb.semana2;
+package br.com.treinaweb;
 
 public class OperadoresLogicos {
     public static void main(String[] args) {
