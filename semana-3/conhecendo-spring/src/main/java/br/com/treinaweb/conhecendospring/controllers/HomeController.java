c4e13757d4622051675a80b257c7e95828ca4883
Samanta Fluture <samantafluture@gmail.com>
2021-11-12 18:31:07 -0200

    create new spring app with jpa and hibernate

--- semana-3/conhecendo-spring/src/main/java/br/com/treinaweb/conhecendospring/controllers/HomeController.java
+++ semana-3/conhecendo-spring/src/main/java/br/com/treinaweb/conhecendospring/controllers/HomeController.java
@@ -1,8 +1,14 @@
 package br.com.treinaweb.conhecendospring.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.treinaweb.conhecendospring.dto.DiaristaForm;
 
 @Controller
 @RequestMapping("/")
@@ -15,4 +21,32 @@ public class HomeController {
         return "home"; // Retorna o nome da view que vai ser executada
     }
 
+    // Recebe como parâmetro
+    @GetMapping("/modelmap")
+    public String home2(ModelMap modelMap) {
+        modelMap.addAttribute("nome", "Samanta");
+        return "home";
+    }
+
+    // Retorno objeto view
+    @GetMapping("/modelandview")
+    public ModelAndView home3() {
+        var modelAndView = new ModelAndView("home");
+        modelAndView.addObject("nome", "Mundo");
+        return modelAndView;
+    }
+
+    // Responsável por exibir a tela do formulário
+    @GetMapping("/form")
+    public ModelAndView form() {
+        var modelAndView = new ModelAndView("form");
+        modelAndView.addObject("form", new DiaristaForm());
+        return modelAndView;
+    }
+
+    @PostMapping("/form")
+    public String form2(DiaristaForm form) {
+        System.out.println(form);
+        return "redirect:/form";
+    }
 }
